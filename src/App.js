c8d290a79dfa13b66148caf9c4da1b779d6bdd62
Python The Hack <lrulvoni@gmail.com>
2022-05-27 20:24:21 +0200

    update username

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Register from './Register';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Register />
       </header>
     </div>
   );
---
+++ src/Register.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useRef, useState, useEffect} from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24} $/;
+
+
+
+const Register = () => {
+
+    const useRef = useRef();
+    const errRef = useRef(); 
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setpwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        useRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Register
